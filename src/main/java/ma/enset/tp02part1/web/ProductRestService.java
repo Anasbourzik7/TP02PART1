faa467195b5044e80e2b4b67bf2b4c9f9cfdf714
macbook pro <macbookpro@MacBook-Pro-de-Paki.local>
2024-04-02 03:38:30 +0000

    First commit

---
+++ src/main/java/ma/enset/tp02part1/web/ProductRestService.java
@@ -0,0 +1,25 @@
+package ma.enset.tp02part1.web;
+
+import ma.enset.tp02part1.entities.Product;
+import ma.enset.tp02part1.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductRestService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public List<Product> products(){
+        return productRepository.findAll();
+    }
+    @GetMapping("/products/{id}")
+    public Product findProduct(@PathVariable Long id){
+        return productRepository.findById(id).get();
+    }
+}
---
+++ src/test/java/ma/enset/tp02part1/Tp02part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.tp02part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp02part1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
